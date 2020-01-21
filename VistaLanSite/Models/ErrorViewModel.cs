9d3312688ab35648e6b6d3d8f858cbf96c0fccf5
Ozzymops <justinmuris@gmail.com>
2020-01-21 14:12:15 +0100

    Add project files.

---
+++ VistaLanSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VistaLanSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
