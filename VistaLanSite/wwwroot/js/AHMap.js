47f1d67a987778e71b3131ebd7f5c6ee8b9609df
Ozzymops <justinmuris@gmail.com>
2020-01-27 16:51:14 +0100

    More OpenStreetMap API stuff added

---
+++ VistaLanSite/wwwroot/js/AHMap.js
@@ -0,0 +1,15 @@
+﻿// OpenStreetMap API
+// Position
+var ahLat = 50.8906913;
+var ahLon = 5.9842102;
+
+// Construct AH map
+ahMap = L.map('ahMap').setView([ahLat, ahLon], 17);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    maxZoom: 17,
+}).addTo(ahMap);
+
+// Markers
+ahMarker = L.marker([ahLat, ahLon]).addTo(ahMap);
+ahMarker.bindPopup("<b>Albert Heijn</b><br />Schandelerboord 25<br />Heerlen").openPopup();
---
+++ VistaLanSite/wwwroot/js/VistaMap.js
@@ -0,0 +1,15 @@
+﻿// OpenStreetMap API
+// Position
+var vistaLat = 50.8800119;
+var vistaLon = 5.9563456;
+
+// Construct VISTA map
+vistaMap = L.map('vistaMap').setView([vistaLat, vistaLon], 16);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    maxZoom: 16,
+}).addTo(vistaMap);
+
+// Markers
+vistaMarker = L.marker([vistaLat, vistaLon]).addTo(vistaMap);
+vistaMarker.bindPopup("<b>VISTA College</b><br />Valkenburgerweg 148<br />Heerlen").openPopup();
